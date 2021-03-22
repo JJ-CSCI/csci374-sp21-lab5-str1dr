1f86368c632f6b27bbd713d6dd0fb477a6953588
Daniel Yadgaroff <daniel.yadgaroff@gmail.com>
2021-03-22 22:34:03 +0000

    worked on the first function

--- main.fs
+++ main.fs
@@ -5,10 +5,18 @@ type Tree =
     | Leaf of int
 
 let rec prod (t:Tree) :int =
-    0
+  let (a,b)
+  match t with
+    | Leaf a -> a // a is int type
+    | Node (a,b) -> a * b // but a & b now of Tree type so use recursion to process them.
+    
+    | _ -> 0
+
+// Tuple denifed as comma separated values
+// (a,b) is a tuple
 
 let rec map (f:int->int) (t:Tree) :Tree =
-    t
+    | 
 
 let rec foldStr (nf:string -> string -> string) (lf:int->string) (t:Tree) :string =
     ""
